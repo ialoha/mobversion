05d0d845565b8652adb40c5e42b9eba61ceba019
ialoha <48326743+ialoha@users.noreply.github.com>
2022-04-29 14:03:33 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,58 @@
+$(window).on('load', function () {
+    $('#preloader').fadeOut('500', function () {
+        $(this).remove();
+    });
+});
+
+
+if ($('.app-header.fixed-top').length > 0) { 
+    $('body').css('padding-top', $('.app-header').outerHeight() + 'px')
+}
+
+
+var last_scroll_top = 0;
+$(window).on('scroll', function(){
+    if( $(this).scrollTop() > 7 ){
+        scroll_top = $(this).scrollTop();
+        
+        // detect scroll
+        if(scroll_top < last_scroll_top) {
+            $('body').removeClass('scrolling-down').addClass('scrolling-up');
+        }
+        else {
+            $('body').removeClass('scrolling-up').addClass('scrolling-down');
+
+        }
+        last_scroll_top = scroll_top;
+
+    } else {
+        $('body').removeClass('scrolling-down scrolling-up');
+    }
+});
+
+
+
+$('.js-check :radio').change(function () {
+    var check_attr_name = $(this).attr('name');
+    if ($(this).is(':checked')) {
+        $('input[name='+ check_attr_name +']').closest('.js-check').removeClass('active');
+        $(this).closest('.js-check').addClass('active');
+       // item.find('.radio').find('span').text('Add');
+
+    } else {
+        item.removeClass('active');
+        
+    }
+});
+
+
+$('.js-check :checkbox').change(function () {
+    var check_attr_name = $(this).attr('name');
+    if ($(this).is(':checked')) {
+        $(this).closest('.js-check').addClass('active');
+       
+    } else {
+        $(this).closest('.js-check').removeClass('active');
+       
+    }
+});
